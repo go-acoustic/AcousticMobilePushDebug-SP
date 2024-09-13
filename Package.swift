6ce3402a9406482b3c75a1da0a65eb699e8a8474
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-09-12 19:24:53 -0700

    Beta AcousticMobilePush build: 3.9.28
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - Fix an issue with Swift Packages for release not having correct checksum and manifest.
    
    Improvements:
    
    - None.
    
    Our environment for this release:
    
    - Xcode 15.2
    - MacOS 14.3.1
    - iOS 12.x to 17.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    	- watch
    		- arm64
    		- arm64_32
    		- armv7k
    		- i386
    		- x86_64
    	- macos
    		- arm64
    		- x86_64-maccatalyst
    		- x86_64-simulator
    
    Known Issues
    
    - AcousticMobilePushWatch.xcframework is not supported in xcode 15 and above, but it works with current AcousticMobilePush.xcframework.

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "AcousticMobilePush",
-            url: "https://github.com/go-acoustic/Acoustic-Mobile-Push-iOS/releases/download/3.9.27/AcousticMobilePush_XCFramework_Debug.zip",
-            checksum: "a44eddb8a2ad9573292c758ea675989830686c37e7ca96e663acb2be4d329617"),
+            url: "https://github.com/go-acoustic/Acoustic-Mobile-Push-iOS/releases/download/3.9.28/AcousticMobilePush_XCFramework_Debug.zip",
+            checksum: "6ef37f74e0b033a5963fa2d24b45657662a1d35204dc467ff427fda843e3d4d3"),
     ]
 )
